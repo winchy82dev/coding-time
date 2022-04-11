c1b284823c6d0c2f848f823fd0af894b277c7b1f
Winchy <nlahkim82@gmail.com>
2022-04-11 12:25:12 +0000

    add challenge 3

--- 8kyu/dayv2-002.js
+++ 8kyu/dayv2-002.js
@@ -23,3 +23,29 @@ const index = (array, n) => (array[n] !== undefined) ? array[n] ** n : -1
 console.log(' ')
 console.log(index([1, 3, 10, 100],3))
 console.log(index([6,31], 3))
+
+//
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+
+function gooseFilter(b) {
+  let result = []
+  let found = false
+  let g = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  for (let i = 0; i < b.length; i++){
+    for (let j = 0; j < g.length; j++){
+    	console.log(`b${i}: ${b[i]}`)
+    	console.log(`g${j}: ${g[j]}`)
+	      if (b[i] === g[j]){
+	      	found = true
+	      }  
+      }
+      if (found === false){
+  	 	result.push(b[i])
+  	  }
+		found = false
+    }
+    return result
+  }
+ 
+console.log(' ')
+console.log(gooseFilter(["African","Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]))
